e3d4040518842562af679e9bcf7130aaeffc6e3d
Jonathan Zerby <jon@pop-os.localdomain>
2021-12-14 14:35:03 -0700

    added more to dots

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,12 @@
-set fish_greeting 
+set fish_greeting
 
 
-######ALIAS HERE
+ 
+#ABBRRIEVIATIONS
+abbr s 'apt-cache search'
+abbr u 'sudo apt-get update && sudo apt-get upgrade'
+abbr store 'io.elementary.appcenter'
 
+##ALIAS 
 alias ll='ls -alF'
+alias q='exit'
